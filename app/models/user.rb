2f51facddc712129382bb2a4d6410a0bca334adf
takahiroppongi <asa.taka325@gmail.com>
2020-09-17 17:47:51 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :nick_name,
+  validates :email,uniqueness: true
+  validates :email,format: { with: /\A\S+@\S+\.\S+\z/ }
+  validates :password,length: { minimum: 6, }
+  validates :password,format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password-confirmation
+  validates :second_name,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :second_name_kana: format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana: format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  get '/users/sign_up', to: 'registrations#new'
 end
