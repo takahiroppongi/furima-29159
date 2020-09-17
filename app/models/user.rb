37812acae2250fc38f7cee23511ed64e702a60f0
takahiroppongi <asa.taka325@gmail.com>
2020-09-17 19:52:42 +0900

    RSpec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-  validates :nick_name,
-  validates :email,uniqueness: true
-  validates :email,format: { with: /\A\S+@\S+\.\S+\z/ }
-  validates :password,length: { minimum: 6, }
-  validates :password,format: { with: /\A[a-zA-Z0-9]+\z/ }
-  validates :password-confirmation
-  validates :second_name,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :second_name_kana: format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana: format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday,
+  validates :nick_name
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :second_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: "is invalid. Input full-width characters."}
+  validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :second_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday
   end
 end
