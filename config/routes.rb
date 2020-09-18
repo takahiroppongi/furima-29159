11f5e3f7252f79332387adf43b04950289ae445a
takahiroppongi <asa.taka325@gmail.com>
2020-09-18 18:27:04 +0900

    修正1(ログイン機能実装、テストコード追加)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  get '/users/sign_up', to: 'registrations#new'
-  delete '/users/sign_out', to: 'sessions#destroy'
+  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nick_name             { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a'+Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     second_name           { '山田' }
     first_name            { '陸太郎' }
