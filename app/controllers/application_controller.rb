b830bfac63ac1d297fd455a53b3b63f65ea3db4e
takahiroppongi <asa.taka325@gmail.com>
2020-09-19 13:22:20 +0900

    リダイレクトの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :move_to_index, except: [:index, :show]
 
   protected
 
@@ -13,6 +14,12 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
+    end
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
     end
   end
 end
