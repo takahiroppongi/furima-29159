e1e5ec5f72e499db46e4f8ba6ab8ed2a14e9204f
takahiroppongi <asa.taka325@gmail.com>
2020-09-22 11:33:30 +0900

    非同期通信で手数料と利益表示

---
+++ app/javascript/price.js
@@ -0,0 +1,15 @@
+function tax() {
+
+  const price = document.getElementById("item-price");
+  price.addEventListener('input', function() {
+    const add_tax = document.getElementById("add-tax-price"); //挿入先の要素
+    const profit = document.getElementById("profit");
+    x = price.value;// priceという要素から数字を取得
+    let calc_tax = x / 10 // これが手数料
+    let calc_profit = x - calc_tax
+    add_tax.innerHTML = calc_tax // これが販売利益
+    profit.innerHTML = calc_profit // 右側に挿入したいものを持ってくる // ここで計算結果を要素に挿入
+      
+  });
+}
+window.addEventListener("load",tax)
