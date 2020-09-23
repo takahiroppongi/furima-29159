ed0e0cb548f39c72643e389ab0507ff609d61d7d
takahiroppongi <asa.taka325@gmail.com>
2020-09-23 18:04:56 +0900

    未ログインユーザーに対する出品画面遷移の防止

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :nemw, :create]
+  before_action :move_to_index, except: [:index]
 
   def index
     @items = Item.all.order('created_at DESC')
