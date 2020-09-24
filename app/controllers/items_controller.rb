e3464ef151675505b5467b38cc727bee176f22ad
takahiroppongi <asa.taka325@gmail.com>
2020-09-24 16:31:00 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def move_to_index
